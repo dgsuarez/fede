6dcc981210df4ea00522f54e9aa11705141f2b1a
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-01-17 16:11:18 +0100

    add stubs to manifest

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='fede',
   description='Fast & Easy Docker Exec',
   author='Diego Guerra',
   author_email='diego.guerra.suarez@gmail.com',
-  py_modules=['fede_exec'],
+  py_modules=['fede_exec', 'fede_stub'],
   scripts=['fede']
   )

51645c6978f2e352581e4856eb130f99774755d9
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-01-17 15:33:46 +0100

    fede all the way

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='figxec',
+setup(name='fede',
   version='0.1',
-  description='simple docker exec',
+  description='Fast & Easy Docker Exec',
   author='Diego Guerra',
   author_email='diego.guerra.suarez@gmail.com',
   py_modules=['fede_exec'],
-  scripts=['figxec']
+  scripts=['fede']
   )

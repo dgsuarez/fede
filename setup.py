7c5f98c43723adc1c9a4a27e7bcd1029a8e1df39
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-01-17 15:18:13 +0100

    make installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='figxec',
+  version='0.1',
+  description='simple docker exec',
+  author='Diego Guerra',
+  author_email='diego.guerra.suarez@gmail.com',
+  scripts=['figxec']
+  )

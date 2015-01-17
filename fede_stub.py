cae80f70e7df934c68bb5ae927f095455430c599
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-01-17 16:05:54 +0100

    basic stub

---
+++ fede_stub.py
@@ -0,0 +1,31 @@
+import os
+import stat
+
+def create_stub_dir():
+  if not os.path.exists(stub_dir_path()):
+    os.makedirs(stub_dir_path())
+
+def stub_dir_path():
+  return ".fede/stubs"
+
+def stub_path(stub_name):
+  return stub_dir_path() + "/" + stub_name
+
+def stub_content(args):
+  command = "fede exec " + " ".join(args)
+  return """
+  #! /bin/sh
+
+  %s
+
+  """ % (command,)
+
+def create_stub(stub_name, args):
+  create_stub_dir()
+  with open(stub_path(stub_name), 'w') as f:
+    f.write(stub_content(args))
+  st = os.stat(stub_path(stub_name))
+  os.chmod(stub_path(stub_name), st.st_mode | stat.S_IEXEC)
+
+def fede_stub(args):
+  create_stub(args[0], args[1:])
